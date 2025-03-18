6d907717dc2bf883183db74cbc77bc398276c854
Rashmi Ranjan Sadangi <72812857+RashmiRanjanSadangi625@users.noreply.github.com>
2025-03-18 21:12:05 +0530

    Update apiConfig.js

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL="http://localhost:3000"
+export const API_BASE_URL="https://fastmart.onrender.com"
 const jwt=localStorage.getItem("jwt");
 
 export const api=axios.create({
