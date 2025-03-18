11036dee83f7f5abc4a2166445fd0888b43a7cc1
Rashmi Ranjan Sadangi <72812857+RashmiRanjanSadangi625@users.noreply.github.com>
2025-03-18 22:03:26 +0530

    Update apiConfig.js

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,6 +1,14 @@
 import axios from "axios"
 
-export const API_BASE_URL="https://fastmart.onrender.com"
+export const API_BASE_URL = 'mongodb+srv://admin:admindb@learning.mh6j7.mongodb.net/featuredb?retryWrites=true&w=majority';
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connection established successfully'))
+.catch(err => console.error('MongoDB connection error:', err));
+
 const jwt=localStorage.getItem("jwt");
 
 export const api=axios.create({
