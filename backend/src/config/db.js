dfcfff8c8e14b3c583baeda4469815956be52cb4
Rashmi Ranjan Sadangi <72812857+RashmiRanjanSadangi625@users.noreply.github.com>
2025-03-18 21:36:28 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ const  mongoose  = require("mongoose");
 // const mongodbUrl="mongodb+srv://rashmi:ecommerce@cluster0.b24gn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 const connectDb = async()=>{
-    return await mongoose.connect("mongodb+srv://rashmi:ecommerce@ecommerce.4dllk.mongodb.net/?retryWrites=true&w=majority&appName=Ecommerce");
+    return await mongoose.connect("mongodb+srv://rashmi:ecommerce@ecommerce.4dllk.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce");
     // console.log(conn);
 }
 
